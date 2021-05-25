a7e6faa9545e8d9ec31803b720532d6f99203db1
rezzaarsdy <rezaarisandy2525@gmail.com>
2021-05-26 00:28:09 +0800

    add file Note Adapter, Note Dao, Note Repository and Note view model

---
+++ app/src/main/java/com/example/noteapprezzaarsdy/NoteViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.noteapprezzaarsdy
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+}
