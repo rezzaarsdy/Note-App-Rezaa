df657e9a88261159adca1de390569647fb57face
rezzaarsdy <rezaarisandy2525@gmail.com>
2021-05-26 00:23:01 +0800

    fix Error files AppDatabase and Note

--- app/src/main/java/com/example/noteapprezzaarsdy/AppDatabase.kt
+++ app/src/main/java/com/example/noteapprezzaarsdy/AppDatabase.kt
@@ -2,27 +2,27 @@ package com.example.noteapprezzaarsdy
 
 import android.content.Context
 import android.provider.ContactsContract
-import android.room.Database
-import android.room.Room
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], exportSchema = false, version = 1)
-abstract class AppDatabase : RoomDatabase(){
-    abstract  fun noteDao() : NoteDao
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
 
-        companion object{
+    companion object {
 
         private const val DB_NAME = "NOTE_DB"
-        private var instance = AppDatabase? = null
+        private var instance: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase? {
-            if (instance == null){
+            if (instance == null) {
                 synchronized(AppDatabase::class) {
                     instance = Room
                         .databaseBuilder(
-                                context,
-                                AppDatabase::class.java,
-                                DB_NAME
+                            context,
+                            AppDatabase::class.java,
+                            DB_NAME
                         )
                         .build()
                 }
